cafd3ee0e4911cc8619049665601c1425fb66270
zccarballo <zchacar.genius@gmail.com>
2025-05-21 13:21:01 +0200

    Zoe esta facendo un commit

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 const express = require("express");
 const path = require("path");
+const cors = require('cors')
 const app = express();
 
-//Paxinas
-//app.use(express.urlencoded)
-
-//app.use(cors())
-app.use(express.urlencoded({extended: true})) //para imprimir datos del form en consola
+// use
+//app.use(cors());
+app.use(express.urlencoded({extended: true}))
 // Accedo o arquivo estático
 app.use(express.static(path.join(__dirname, "static")));
-app.post("/acceso", (req, res) => { //carga en server la app si le doy a enviar en el form
-    // console.log(req.body); //imprimir en consola
+app.post("/acceso", (req, res) => {
+    const {nome, email} = req.body;// desesctructura o obxeto de entrada
+    console.log(nome, email);
     if(req.body.nome == 'Zoe' && req.body.email == 'yo@gmail.com'){
-    res.sendFile(path.join(__dirname, "static/views/app.html"));
+        res.sendFile(path.join(__dirname, "/static/views/app.html"));
     }else{
-    res.send("<h1>Usuario no autorizado</h1>");
+        res.send("<h1>Usuario no autorizado</h1>"); 
     }
+     
 });
 app.get("/enviousuario", (req, res) => {
     res.send({usuario:{
         nome:'Zoe'
     }})
-}
-)
+})
 //START SERVER
-app.listen(3000, function () { //crea el servidor para navegar en localhost:3000
+app.listen(3000, function () {
  console.log("Server running");
 });
-
