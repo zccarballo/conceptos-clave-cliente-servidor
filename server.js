152330ec9d205eab24d6a0240ca51416f05ee8b8
zccarballo <zchacar.genius@gmail.com>
2025-05-21 13:41:14 +0200

    commit actualizado

--- server.js
+++ server.js
@@ -5,24 +5,35 @@ const app = express();
 
 // use
 //app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 // Accedo o arquivo estático
 app.use(express.static(path.join(__dirname, "static")));
 app.post("/acceso", (req, res) => {
     const {nome, email} = req.body;// desesctructura o obxeto de entrada
     console.log(nome, email);
-    if(req.body.nome == 'Zoe' && req.body.email == 'yo@gmail.com'){
-        res.sendFile(path.join(__dirname, "/static/views/app.html"));
+    let condicionUsuarioCorrecto = req.body.nome == 'Israel' && req.body.email == 'israel@israel.com';
+    let datoEnviadoCondicionUsuarioCorrecto = {
+                resposta:"acesso autorizado",
+                usuario:{
+                    nome:'Israel'
+                }
+            }
+    let datoEnviadoEnErro = {resposta:"faltan campos ou usuario non rexistrado"}
+
+
+    if(condicionUsuarioCorrecto){    
+        res.send(datoEnviadoCondicionUsuarioCorrecto);
     }else{
-        res.send("<h1>Usuario no autorizado</h1>"); 
+        res.send(datoEnviadoEnErro); 
     }
      
 });
-app.get("/enviousuario", (req, res) => {
-    res.send({usuario:{
-        nome:'Zoe'
-    }})
+app.get("/app",(req,res)=>{
+    res.sendFile(path.join(__dirname, "static/views/app.html"));
 })
+
+
 //START SERVER
 app.listen(3000, function () {
  console.log("Server running");
